0b05f08fe039fa36e70c3cbfee43b3723e12709b
Telman Rzaev <rzaev.telman.tarik@gmail.com>
2020-04-21 20:19:31 +0300

    Fix bugs

--- retag.sh
+++ retag.sh
@@ -13,23 +13,26 @@ unset GATHER_POINTS FORMAT
 read -p "Are your shoure want to retag all files (Y|n) -> "
 if [ ! $REPLY == 'Y' ]; then
 	echo "Cancel retag programm"
-	exit
+	exit 1
 fi
 
 echo "The program is continue"
+
+# If buffer is not exist, make it
+if [ ! -d "$BUFFER" ]; then
+	mkdir -p "$BUFFER"
+	echo "dir $(basename $BUFFER) was created"
+fi
 
 dir_before=
 mover () {
 	for dir in $1/*; do
 		if [ -d "$dir" -a ! "$dir" == "$BUFFER" ]; then
-			#echo $dir
 			dir_before+=":$dir"
 			mover "$dir"
 			rm -d "${dir_before##*:}"
-			#echo rm -d $(basename "${dir_before##*:}")
 			dir_before=${dir_before%:*}
 		elif [ ! "$dir" == "$BUFFER" ]; then
-			#echo move $(basename "$dir") 
 			mv "$dir" "$BUFFER"
 		fi
 	done
